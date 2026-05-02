757010b4c722100045e8dc1bf621ceeaa561f696
Timeo <timeo.canto--ringelstein@etu.univ-cotedazur.fr>
2026-05-03 01:19:16 +0200

    refactor: useless div

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,13 @@
 import Hero from "@/src/components/home/Hero";
-import BlogCard from "@/src/components/BlogCard";
 import BlogCardList from "@/src/components/BlogCardList";
 
 export default function Home() {
-  return (
-    <main className="relative flex justify-center items-center">
-      <div className="">
-          <Hero />
-          <BlogCardList />
-      </div>
-    </main>
-  );
+    return (
+        <main className="relative flex justify-center items-center">
+            <div className="">
+                <Hero />
+                <BlogCardList />
+            </div>
+        </main>
+    );
 }
