44c2e5635d87f0f749fc3ae47b093d7ce953e47f
Timeo <timeo.canto--ringelstein@etu.univ-cotedazur.fr>
2026-05-03 23:16:53 +0200

    feat: adding a chibi figure for interactiveness

--- src/components/home/Hero.tsx
+++ src/components/home/Hero.tsx
@@ -4,18 +4,33 @@ import Image from "next/image";
 
 const Hero = () => {
   return (
-    <div className="pb-20 pt-40">
+    <div className="
+        pb-20 pt-40
+        flex justify-between items-center
+    ">
         <div>
             <h3>Disponible pour alternance Côte d&#39;Azur</h3>
-            <h1>Ingénieur, curieux & créatif.</h1>
+
+            <h1 className="
+                font-serif text-6xl font-semibold tracking-tighter"
+            >
+                Ingénieur, <br/>
+                <span className={"italic text-terracotta"}>curieux</span> & créatif.
+            </h1>
+
             <h2>Élève ingénieur en informatique passionné par l&#39;intelligence artificielle.</h2>
-            <CremeButton>Voir mes projets</CremeButton>
-            <CremeButton>Télécharger le CV</CremeButton>
+
+            <div className={"flex gap-4"}>
+                <CremeButton>Voir mes projets</CremeButton>
+                <CremeButton>Télécharger le CV</CremeButton>
+            </div>
+
         </div>
+
         <img
             src="/chibi_cloud.png"
             alt={"chibi on a cloud"}
-            className={"-scale-x-100"}
+            className={"h-50 -scale-x-100"}
         />
     </div>
   );
