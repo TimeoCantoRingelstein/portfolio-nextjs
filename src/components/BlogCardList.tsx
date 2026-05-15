124fa0310991c0c2a59cd3dc2fc1005b70d6c3a9
Timeo <timeo.canto--ringelstein@etu.univ-cotedazur.fr>
2026-05-15 12:50:45 +0200

    fix: Rename 'titre' prop to 'title' in BlogCardList

--- src/components/BlogCardList.tsx
+++ src/components/BlogCardList.tsx
@@ -6,17 +6,17 @@ const BlogCardList = () => {
         <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 lg:grid-cols-3 items-stretch p-6">
             <BlogCard
                 date={"Avril 2026"}
-                titre={"Test"}
+                title={"Test"}
                 description={"Ceci est une description"}
                 tags={["hi", "hey", "hoy"]}
             />
             <BlogCard
                 date={"Mai 2026"}
-                titre={"Test"}
+                title={"Test"}
             />
             <BlogCard
                 date={"Janvier 2026"}
-                titre={"Test"}
+                title={"Test"}
             />
         </div>
     );
